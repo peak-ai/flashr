2d31a1ef74f0391fa35ba98fe729b85db9faa3df
Akshay Jain <akshay.jain@peak.ai>
2022-11-11 14:38:00 +0530

    feat: add support for react elements in flash message

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,8 @@
+import React from 'react';
 import { ADD_MESSAGE, CLEAR_MESSAGE, CLEAR_ALL_MESSAGES } from './constants';
 import { Message } from './factory';
 
-export type AddMessageParams = Partial<Message> & { message: string };
+export type AddMessageParams = Partial<Message> & { message: string | React.ReactNode };
 export interface AddMessageAction {
   type: typeof ADD_MESSAGE;
   payload: AddMessageParams;
